b73d37ce009e916e114c9e3313bba68902c41305
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-07-30 11:45:04 +0300

    add question description

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-// create a function that take user_id as a param and get all posts and filter it by user_id
+// 1- create a function that take user_id as a param and get all posts and filter it by user_id
 // hint: use async/await try-catch and fetch api and also (fetch and foreach)
 
 
-const sendData = async () => {
-  const data = {
-    name: "Ahmed",
-    email: "ahmed@example.com"
-  };
-
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const result = await response.json();
-    console.log("Success:", result);
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-};
-
-sendData();
+// 2. Send JSON Data via Fetch POST Request 
+// • Use the fetch() API to send a POST request with JSON data to an 
+// API endpoint. Log the response or success message. 

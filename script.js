a35712565830b9e97fa92f560e5768e4cbba9120
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-07-31 11:58:34 +0300

    set description of challenges

--- script.js
+++ script.js
@@ -1,62 +1,12 @@
-// 1- create a function that take user_id as a param and get all posts and filter it by user_id
-// hint: use async/await try-catch and fetch api and also (filter and foreach)
-// requirement: check if no post exist by user_id
+// 1 - make http request to get all users then get the first user and log its info (name, email)
+// after that, find posts by his/her id and log its info
+// Task: try make it without try-catch  
 
-// async function getPostsByUserId (user_id) {
-//   try {
-//     const request = await fetch('https://jsonplaceholder.typicode.com/posts')
-//     const data = await request.json()
 
-//     // const result = []
-//     // for (let i = 0; i < data.length; i++) {
-//     //     if (data[i].user_id === user_id){
-//     //         result.push(data[i])
-//     //     }
-//     // }
-//     let postsByUserId = data.filter(post => post.userId === user_id)
-
-//     // for (let i = 0; i < postsByUserId.length; i++) {
-//     //     const {userId, id, title} = postsByUserId[i]
-//     //     console.log(`userId #${userId} - ${id} ${title}`)
-//     // }
-
-//     postsByUserId.forEach(post => {
-//       const { userId, id, title } = post
-//       console.log(`userId #${userId} - ${id} ${title}`)
-//     })
-//   } catch (error) {
-//     console.log(`SOMETHING GOES WRONG ${error.message}`)
-//   }
-// }
-
-// getPostsByUserId(12121)
-
-// 2. Send JSON Data via Fetch POST Request
-// • Use the fetch() API to send a POST request with JSON data
-// to an API endpoint. Log the response or success message.
-// create html form and make 2 input (name, email) and take those data using DOM
-
-// async function makePOSTRequestToSendData () {
-//   // https://jsonplaceholder.typicode.com/posts
-//   const data = {
-//     name: 'Ahmed',
-//     email: 'a.sadek@gmail.com'
-//   }
-
-//   try {
-//     const request = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(data)
-//     })
-
-//     const response = await request.json();
-//     console.log(response)
-//   } catch (error) {
-//     console.log(`SOMETHING GOES WRONG ${error.message}`)
-//   }
-// }
-
-// makePOSTRequestToSendData();
+// 2- create class for PostManager that has (user allPosts) and properties (user, allPost and posts for this user)
+// - make loadPosts method that log the number of posts related to this user
+// - make display user info method that display in log (username and email)
+// - make display post titles method that display post id and post titles for posts related to user,
+// make check if posts related to user are not empty
+// - make method that get user_post by id, and return null if not exist
+// Task: separate User and posts by make each one in single class and link to then, prevent reaching from user_posts or user property

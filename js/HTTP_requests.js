6f468119570ca6afac16723139ad00497e9c8482
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-08-02 14:13:06 +0300

    slove challenge_2_8 question

---
+++ js/HTTP_requests.js
@@ -0,0 +1,25 @@
+// data {
+// title: "",
+// body: "",
+// userId: ""
+// } => Object
+// function makePOSTRequest (url, data) {
+//   fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   })
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(result => {
+//       console.log('Success:', result)
+//     })
+//     .catch(error => {
+//       console.log(`Error: ${error.message}`)
+//     })
+// }
+
+// export default makePOSTRequest
--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@
 // try to get data by id and edit on it
 
 
+// import makePOSTRequest from './HTTP_requests.js';
+
+// const data1 = {
+//     userId: 1,
+//     title: "Hello, world",
+//     body: "this is post body as a testing"
+// }
+// const data2 = {
+//     userId: 3,
+//     title: "Hello, world from data 2",
+//     body: "this is post body as a testing from data2"
+// }
+// const BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+// makePOSTRequest(BASE_URL, data1)
+// makePOSTRequest(BASE_URL, data2)

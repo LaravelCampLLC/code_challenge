8bf94c7950e1ac23710b4888e059c40f182bfa36
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-08-02 13:15:16 +0300

    set description for challenge_2_8

--- js/main.js
+++ js/main.js
@@ -1,33 +1,10 @@
-// 1 - make http request to get all users then get the first user and log its info (name, email)
-// after that, find posts by his/her id and log its info
-// Task: try make it without try-catch
-
-// ENDPOINT: https://jsonplaceholder.typicode.com/users => users
-// https://jsonplaceholder.typicode.com/posts => posts
-// import fetchUserAndPosts from './HTTP_requests.js'
-
-// fetchUserAndPosts()
-
+// Send JSON Data via Fetch POST Request 
+// • Use the fetch() API to send a POST request with JSON data to an 
+// API endpoint. Log the response or success message. 
+// BASE_URL: https://jsonplaceholder.typicode.com/posts
+// (((TASK))) 
+// make html form and when submit get the data instead of make it static
+// with try-catch, 
+// try to get data by id and edit on it
 
 
-// 2- create class for PostManager that has (user allPosts) and properties (user, allPost and posts for this user)
-// - make loadPosts method that log the number of posts related to this user
-// - make display user info method that display in log (username and email)
-// - make display post titles method that display post id and post titles for posts related to user,
-// make check if posts related to user are not empty
-// - make method that get user_post by id, and return null if not exist
-// Task: separate User and posts by make each one in single class and link to then, prevent reaching from user_posts or user property
-
-// import {user, posts as allPosts} from './static.js'
-// import PostManager from './post_manager.js'
-
-// const manager1 = new PostManager(user, allPosts);
-// manager1.loadPosts();
-// console.log("#######")
-// manager1.displayUserInfo()
-// console.log("#######")
-// manager1.displayPostsTitleByUser()
-// console.log("#######")
-// console.log(manager1.getPostById(1))
-// console.log("#######")
-// console.log(manager1.getPostById(10))
--- js/static.js
+++
@@ -1,7 +0,0 @@
-export const user = { id: 1, name: "Aya", email: "aya@example.com" };
-
-export const posts = [
-  { id: 1, userId: 1, title: "Aya’s first post" },
-  { id: 2, userId: 1, title: "Learning OOP in JS" },
-  { id: 3, userId: 2, title: "Ahmed’s post" }
-]
